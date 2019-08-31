fd68ea5a5cebf0d22b05c9e1b9e0a894052dbacc
jabrane <jabrane.pro@gmail.com>
2019-08-30 20:30:12 -0400

    Query of datatable of passing order

--- application/models/Produit.php
+++ application/models/Produit.php
@@ -74,4 +74,22 @@ function productSearchCount($search,$start,$length,$cat,$sort,$direction)
   return $query->num_rows();
 } 
 
+public function getListOfProductWithquantite()
+{
+  $this->db->select('pr.codeArticle,pr.description,pr.prix,pr.quantite-coalesce(sum(ln.quantiteAchat),0) qte_disp, coalesce(sum(ln.quantiteAchat),0) qte,cat.nomCategorie categorie');
+  $this->db->from('produit pr'); 
+  $this->db->join('lignefacture ln', 'pr.codeArticle = ln.codeArticle', 'left');
+  $this->db->join('categorie cat', 'cat.idCategorie = pr.idCategorie');
+  $this->db->group_by('ln.codeArticle'); 
+  $this->db->order_by('pr.idCategorie','DESC');         
+  $query = $this->db->get(); 
+  if($query->num_rows() != 0)
+  {
+    return $query->result_array();
+  }
+  else
+  {
+    return false;
+  }
+}
 }
